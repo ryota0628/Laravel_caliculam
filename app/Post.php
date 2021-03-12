232d955aa985ecebbe5fcbc1abf99ebafdbb5310
ryota0628 <ryota_1dan@live.jp>
2021-03-12 20:43:30 +0900

    8-4完了

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+    'title',
+    'body',
+    ];
     public function getPaginateByLimit(int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitでページ制限をかける
